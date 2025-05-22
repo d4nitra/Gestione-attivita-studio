8cf45837c450a6649b01b35a08a854522a540c9f
d4nitra <d.ruocco10@studenti.unisa.it>
2025-05-22 13:25:47 +0200

    Implementazione della funzione relativa all'aggiornamento del progresso delle singole attività

--- main.c
+++ main.c
@@ -13,7 +13,7 @@
 #include "attivita.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>  
 /*
 --------------------------------------------------------
 |  Funzione: menu
@@ -23,13 +23,14 @@
 --------------------------------------------------------
 */
 void menu() {
-    printf("\n---- Gestione Attività di Studio ----\n");
-    printf("1. Aggiungi Attività\n");
-    printf("2. Visualizza Attività\n");
-    printf("3. Rimuovi Attività\n");
-    printf("4. Monitoraggio del Progresso\n");
-    printf("5. Genera Report Settimanale\n");
-    printf("6. Esci\n");
+    printf("\n---- Gestione Attività di Studio ----\n Inserisci il numero corrispondente per eseguire un azione: \n");
+    printf("1. Aggiungi attività\n");
+    printf("2. Visualizza tutte le attività\n");
+    printf("3. Rimuovi un'attività inserendo la sua descrizione\n");
+    printf("4. Monitoraggio del progresso\n");
+    printf("5. Visualizza Report Settimanale\n");
+    printf("6. Aggiorna ore svolte di un'attività\n");
+    printf("7. Esci\n");
     printf("Scelta: ");
 }
 
@@ -48,18 +49,12 @@ int main() {
     ListaAttivita lista = creaLista(); // Creo la lista di attività
     int scelta;
     char descrizione[MAX_DESC];
+    int ore;
 
     do {
         menu();
         scanf("%d", &scelta);
         getchar(); // Pulizia del buffer
-       while(scelta<1||scelta>6)
-		{
-		printf("L'input inserito non risulta valido, per favore inserire un numero da 1 a 6");
-         menu();
-        scanf("%d", &scelta);
-        getchar();
-  	}
 
         switch (scelta) {
             case 1: {
@@ -89,14 +84,24 @@ int main() {
                 generaReportSettimanale(lista);
                 break;
             case 6:
+                // Aggiornamento ore svolte
+                printf("Inserisci la descrizione dell'attività da aggiornare: ");
+                fgets(descrizione, MAX_DESC, stdin);
+                descrizione[strcspn(descrizione, "\n")] = 0;
+                printf("Inserisci il numero di ore svolte da aggiungere: ");
+                scanf("%d", &ore);
+                getchar(); // pulizia del buffer
+                aggiornaAttivita(lista, descrizione, ore);
+                break;
+            case 7:
                 // Uscita e deallocazione della memoria
                 liberaMemoria(&lista);
                 printf("Uscita dal programma...\n");
                 break;
             default:
-                printf("Scelta non valida!\n");
+                printf("L'input inserito non risulta valido, per favore inserire un numero da 1 a 7\n");
         }
-    } while (scelta != 6);
+    } while (scelta != 7);
 
     return 0;
 }
